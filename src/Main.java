724ed7782eaaa57e256b472a17a5b4f0f07ee05d
still-not-telling <JR212384@reigate.ac.uk>
2021-11-04 12:59:35 +0000

    added stuff

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         ArrayList<String> toppings = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         int size = 0;
-
+        double price = 0.00;
         System.out.println("what size pizza do you want? (small: 1,medium: 2,large: 3)");
         while (!(size <= 3 && size >= 1)) {
             try {
@@ -44,13 +44,38 @@ public class Main {
         while (true) {
             if (input.next() == toppings.get(0)) {
                 System.out.println("you cannot have the same topping twice");
+
+
             } else if (!(input.hasNextInt())) {
                 toppings.add(input.next());
                 break;
             } else {
                 System.out.println("please input an acceptable topping");
+
+
             }
         }
+
+        while (true) {
+            System.out.println("would you like any extra toppings? extra toppings cost £1.25 each");
+            if (input.next().equals("no")) {
+                break;
+            } else if (!(toppings.contains(input.next()))) {
+                toppings.add(input.next());
+                input.next();
+            } else {
+                System.out.println("you cannot pick the same topping twice");
+                input.next();
+            }
+        }
+        if (size == 1) {
+            System.out.println("your pizza is a small ");
+        } else if (size == 2) {
+            System.out.println("your pizza is a medium ");
+        } else {
+            System.out.println("your pizza is a large ");
+        }
+        System.out.println("with toppings: " + toppings.listIterator());
     }
 }
 
